d58c14475197c81d46f5d1d11efdc653d55775c8
Brian Fraser <brian.fraser@booking.com>
2020-11-21 19:12:25 +0100

    Removed Ruby

--- cmake/libprotoc.cmake
+++ cmake/libprotoc.cmake
@@ -17,7 +17,6 @@ set(libprotoc_files
   ${protobuf_source_dir}/src/google/protobuf/compiler/js/js_generator.cc
   ${protobuf_source_dir}/src/google/protobuf/compiler/plugin.cc
   ${protobuf_source_dir}/src/google/protobuf/compiler/plugin.pb.cc
-  ${protobuf_source_dir}/src/google/protobuf/compiler/ruby/ruby_generator.cc
   ${protobuf_source_dir}/src/google/protobuf/compiler/subprocess.cc
   ${protobuf_source_dir}/src/google/protobuf/compiler/zip_writer.cc
 )
